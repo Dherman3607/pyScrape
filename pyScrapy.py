1098fc18cf187e262c2e640cf476b5cf3c9d33c6
david <david@hermannc.dev>
2021-09-29 18:02:46 -0400

    working non-recursive version, with concurrency

--- pyScrapy.py
+++ pyScrapy.py
@@ -1,36 +1,56 @@
 #! /usr/bin/python3
-
+#written by David Herman
 import threading
 
 import argparse
 import requests
 import regex
+import concurrent.futures
+from itertools import repeat
 
 
+def scrape(url):
+	print(f"In scrape, the target is {url}")
+	#successful_URL = ""
+	session = requests.Session()
 
-def scrape(args,session):
-	successful_URL = []
-	for word in args.wordlist:
-		word = word.strip()
-		url=f"{args.url}/{word}"
-		#print(url)
-		r = session.get(url)
-		#
-		if(regex.match(r"(?!403)4(\d\d)",str(r.status_code))):
-			pass	
-		else: 
-			print(f"{url} responds with {r.status_code}")
-			successful_URL.append(url)
-			scrape(args,session)
-	print(successful_URL)
+	r = session.get(url)
+	return r
+
+def read_wordlist(args):
+	wordlist = args.wordlist
+	futures_list = []
+	print(f"in read_wordlist {wordlist}")
+	with concurrent.futures.ThreadPoolExecutor(args.workers) as executor:
+		for word in wordlist:
+			print(f'in read_wordlist the word is {word}')
+			url = f"{args.url}/{word.strip()}"
+			futures = executor.submit(scrape,url)
+			futures_list.append(futures)
+		#print(f"in read_wordlist {futures}")
+	return futures_list
 
 def main():
 	parser = argparse.ArgumentParser(description='Scrape a website directory')
 	parser.add_argument('url', help='the url to scrape')
 	parser.add_argument('wordlist',type=argparse.FileType('r'), help=' The wordlist to scrape with',default="./directory.txt")
-
-	s = requests.Session()
+	parser.add_argument('--workers',type=int, help='Number of threads to use',default=10,required=False)
 	args = parser.parse_args()
-	scrape(args,s)
+	#First pass of the word list
+	futures = read_wordlist(args)
+	successful_URL = []
+	for future in futures:
+		if(regex.match(r"(?!403)[3-5](\d{2})",str(future.result().status_code))): 
+		#or not regex.search(r"(?<=\.)\w+",url_last[-1])):
+			print(F"this url failed {future.result().url} with {future.result().status_code}	")
+		
+		else: 
+			#print(successful_URL)
+			successful_URL.append(future.result().url)
+
+	print(str(successful_URL))
+
+
+
 if __name__ == "__main__":
 	main()
